7d1179e44bd0ac580ca95fdb0a645bf4267d399a
Malik Abd <mohammedabdmalik@gmail.com>
2017-07-13 02:28:00 +0530

    Fixed the state issue

--- src/Actions/AuthActions.js
+++ src/Actions/AuthActions.js
@@ -1,29 +1,42 @@
 import {
   EMAIL_CHANGED,PASSWORD_CHANGED,LOGIN_USER_SUCCESS,LOGIN_USER_FAIL, LOGIN_USER,
-  EMPLOYEE_UPDATE,EMPLOYEE_CREATE,EMPLOYEES_FETCH_SUCCESS, EMPLOYEE_SAVE_SUCCESS
+  EMPLOYEE_UPDATE,EMPLOYEE_CREATE,EMPLOYEES_FETCH_SUCCESS, EMPLOYEE_SAVE_SUCCESS,
+
 } from './types';
 import firebase from 'firebase';
 import {Actions} from 'react-native-router-flux';
 
 
 export const employeeUpdate = ({prop,value}) => {
-  console.log("im in update");
   return{
   type : EMPLOYEE_UPDATE,
   payload : {prop , value}
 };
 };
+
+
 export const employeeCreate = ({name,phone,shift}) => {
    const {currentUser}= firebase.auth();
    return(dispatch) => {
    firebase.database().ref(`/users/${currentUser.uid}/employees`)
     .push({name,phone,shift})
     .then(() => {
-      dispatch({type: EMPLOYEE_CREATE});
-      Actions.employeelist({type:'reset'});
-  });
+      dispatch({type:EMPLOYEE_CREATE});
+    });
  };
 };
+
+export const employeeSave = ({ name, phone, shift, uid }) => {
+  const {currentUser} = firebase.auth();
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+      .set({ name, phone, shift })
+      .then(() => {
+        dispatch({type:EMPLOYEE_SAVE_SUCCESS });
+      });
+  };
+};
+
 
 export const employeesFetch=()=> {
   const {currentUser}= firebase.auth();
@@ -35,20 +48,6 @@ export const employeesFetch=()=> {
   };
 };
 
-export const employeeSave = ({ name, phone, shift, uid }) => {
-  const { currentUser } = firebase.auth();
-console.log("im in employee save");
-  return (dispatch) => {
-    console.log({uid});
-    firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
-      .set({ name, phone, shift })
-      .then(() => {
-        console.log({uid});
-        Actions.employeelist({type:'reset'});
-        dispatch({type:EMPLOYEE_SAVE_SUCCESS });
-      });
-  };
-};
 
 //below are the different action creators
 export const emailChanged = (text) => {
